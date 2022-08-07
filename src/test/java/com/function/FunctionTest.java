06b4cca7a73da83c6c42ec472cf5c390361144a6
coderZzc <669296585@qq.com>
2022-08-07 11:13:49 +0800

    一个前端基于Vue2，后端基于TypeJavaScript的Azure Functions 程序

---
+++ src/test/java/com/function/FunctionTest.java
@@ -0,0 +1,22 @@
+package com.function;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Unit test for Function class.
+ */
+public class FunctionTest {
+    /**
+     * Unit test for hello method.
+     */
+    @Test
+    public void testHello() throws Exception {
+        final Function function = new Function();
+
+        final String ret = function.hello("function", null);
+
+        assertEquals("Hello, function!", ret);
+    }
+}
